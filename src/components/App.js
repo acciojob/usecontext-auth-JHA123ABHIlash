9d4a89815000677b18fb7d89c714932b412a4eca
Abhilash Jha <abhilashjha121@gmail.com>
2026-06-09 08:12:48 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
-
 import React, { useContext } from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
 import { AppContext } from "./context/AppContext";
 
 const App = () => {
-  const {check,handleChange}=useContext(AppContext);
+  const { check, handleChange } = useContext(AppContext);
   return (
     <div>
-        {check ? "You are now authenticated,you can proceed" :"you are not authenticated"}
-        <br></br>
-       <input type="checkbox" id="chx"  checked={check} onChange={handleChange}/>
-       <label htmlFor="chx">I'm not a robot</label>
-    </div>
-  )
-}
+      {check ? (
+        <p className="authText">You are now authenticated,you can proceed</p>
+      ) : (
+        <p>you are not authenticated</p>
+      )}
 
-export default App
+      <input type="checkbox" id="chx" checked={check} onChange={handleChange} />
+      <label htmlFor="chx">I'm not a robot</label>
+    </div>
+  );
+};
+
+export default App;
